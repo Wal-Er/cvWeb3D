374a30d306e1306089aa07780f4c1ecde88da475
Walery Demidko <walerydz@gmail.com>
2025-04-04 21:44:58 +0200

    JS page turn z-index

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,8 +8,14 @@ pageTurnBtn.forEach((el, index) => {
 
       if (pageTurn.classList.contains('turn')) {
          pageTurn.classList.remove('turn');
+         setTimeout(() => {
+            pageTurn.style.zIndex = 20 - index;
+         }, 500);
       } else {
          pageTurn.classList.add('turn');
+         setTimeout(() => {
+            pageTurn.style.zIndex = 20 + index;
+         }, 500);
       }
    }
 });
