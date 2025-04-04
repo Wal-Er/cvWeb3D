56eb020aedf815a41d1b0666f581ffd790876b62
Walery Demidko <walerydz@gmail.com>
2025-04-04 21:23:42 +0200

    JS page turn condition

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,18 @@
 //turn pages when click next or prev button
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((el, index) => {
+   el.onclick = () => {
+      const pageTurnId = el.getAttribute('data-page');
+      const pageTurn = document.getElementById(pageTurnId);
+
+      if (pageTurn.classList.contains('turn')) {
+         pageTurn.classList.remove('turn');
+      } else {
+         pageTurn.classList.add('turn');
+      }
+   }
+});
 //contact me button when click
 //create reverse index function
 //back profile button when click
